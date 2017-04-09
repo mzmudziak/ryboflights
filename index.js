8768fd536169f9676c687ca8bb70b1d3cfa0c68a
Michał Żmudziak <Micha mudziak>
2017-04-09 08:17:27 +0200

    Minor changes and refactoring

--- index.js
+++ index.js
@@ -4,16 +4,51 @@ var request = require('request');
 var _ = require('lodash');
 var config = require('./config');
 var request_promise = require('request-promise');
+var moment = require('moment');
 var fs = require('fs');
-
+var inquirer = require('inquirer');
+var request = require('request');
+var opts = {};
 (function () {
     console.log(config);
 })();
 
+var prompts = [
+    {
+        type: 'input',
+        message: 'How many people?',
+        name: 'amountOfPeople',
+        default: 3,
+        validate: function (input) {
+            return input !== null && input !== undefined && !isNaN(parseFloat(input)) && isFinite(input);
+        }
+    }
+];
+(function () {
+    var done = false;
+    inquirer.prompt(prompts).then(function (answers) {
+        opts.amountOfPeople = answers.amountOfPeople;
+        console.log(opts.amountOfPeople);
+        done = true;
+    });
+    if (done === true)
+    request.post(options, function (error, response, body) {
+        console.log(error);
+        console.log(response);
 
+    
+        fs.writeFile(filename(), JSON.stringify(response, null, 4), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log('The file was saved to: ' + fileName);
+        });
+    }); 
+})();
 
-
-var request = require('request');
+function filename(){
+    return moment.unix(new Date()) + - 'response.json';
+}
 
 var options = {
     uri: config.bin_post + '?key=' + config.google_api_key,
@@ -35,18 +70,4 @@ var options = {
         }
     }
 }
-
-request.post(options, function (error, response, body) {
-    console.log(error);
-
-    console.log(response);
-
-    var fileName = new Date().toUTCString().replace(/T/, '-').replace(/\..+/, '') + '-response.json';
-    fs.writeFile(fileName, JSON.stringify(response, null, 4), function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log('The file was saved to: ' + fileName);
-    });
-});
 
